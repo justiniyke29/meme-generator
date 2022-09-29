af9564096d7a6a13fec3ff02e750fff523ca90df
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-09-29 11:51:08 +0100

    added build

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const  Header= () => {
             <img 
                 src="./images/troll-face.png" 
                 className="header--image"
+                alt="trollFace"
             />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
